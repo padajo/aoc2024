1b381bee8d37c01553a07a887f8e063424c32440
Paul Johnston <pauljohnstonmk@gmail.com>
2024-12-13 13:21:23 +0000

    solution for day02

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -28,6 +28,12 @@ pub mod aoc2024 {
         Ok(data)
     }
 
+    pub fn line_to_number_vec(line: &str) -> Vec<i32> {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        let nums = parts.iter().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+        nums
+    }
+
     // mod for day01
     pub mod day01 {
 
@@ -161,12 +167,96 @@ pub mod aoc2024 {
         
         use std::io::{self};
 
+        fn is_line_safe(line: &str) -> bool {
+
+            let levels = super::line_to_number_vec(line);
+
+            // all numbers must either be higher or lower than the previous number
+            // numbers must not be more than 3 apart
+            let mut prev = levels[0];
+            // first check if first and second numbers are increasing or decreasing
+            let mut increasing = levels[1] > levels[0];
+            // get vec with all but first number
+            let rest = &levels[1..];
+            for level in rest {
+                // get the difference between the current and previous number
+                let current_level = *level;
+
+                if (current_level > prev) != increasing {
+                    return false;
+                }
+
+                let diff = i32::abs_diff(current_level, prev);
+
+
+                if diff > 3 || diff < 1 {
+                    return false;
+                }
+
+                prev = *level;
+            }
+
+            return true;            
+            
+        }
+
+        fn get_safe_line_count(lines: Vec<String>) -> u32 {
+            let mut count = 0;
+            for line in lines {
+                if is_line_safe(&line) {
+                    count += 1;
+                }
+            }
+            count
+        }
+
+        fn is_line_with_dampener_safe(line: &str) -> bool {
+
+            let levels = super::line_to_number_vec(line);
+
+            // check original levels
+            if is_line_safe(line) {
+                return true;
+            }
+
+            // now remove a single level (in order) and check if the line is safe
+            for i in 0..levels.len() {
+                let mut new_levels = levels.clone();
+                new_levels.remove(i);
+                let new_line = new_levels.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(" ");
+                if is_line_safe(&new_line) {
+                    return true;
+                }
+            }
+
+
+            return false;
+            
+        }
+
+        fn get_safe_problem_dampener_count(lines: Vec<String>) -> u32 {
+            let mut count = 0;
+            for line in lines {
+                if is_line_with_dampener_safe(&line) {
+                    count += 1;
+                }
+            }
+            count
+        }
+
         pub fn part1(test: bool) -> Result<u32, io::Error> {
-            Ok(0)
+
+            let lines = super::get_data(test);
+
+            let count = get_safe_line_count(lines.unwrap());
+            Ok(count)
         }
     
         pub fn part2(test: bool) -> Result<u32, io::Error> {
-            Ok(0)
+            let lines = super::get_data(test);
+
+            let count = get_safe_problem_dampener_count(lines.unwrap());
+            Ok(count)
         }
     }
 }
@@ -176,14 +266,13 @@ pub mod aoc2024 {
 
 fn main() {
 
+    // https://adventofcode.com/2024/day/2
 
     println!("Part 1");
-    // let num1 = aoc2024::day01::part1(false).unwrap();
     let num1 = aoc2024::day02::part1(false).unwrap();
     println!("Answer: {}", num1);
 
     println!("Part 2");
-    // let num2 = aoc2024::day01::part2(false);
     let num2 = aoc2024::day02::part2(false);
     match num2 {
         Ok(n) => println!("Answer: {}", n),
@@ -203,12 +292,12 @@ mod tests {
     fn test_part1() {
         // check if part1 returns 11
         let num = aoc2024::day02::part1(true).unwrap();
-        assert_eq!(num, 0);
+        assert_eq!(num, 2);
     }
 
     #[test]
     fn test_part2() {
         let num = aoc2024::day02::part2(true).unwrap();
-        assert_eq!(num, 0);
+        assert_eq!(num, 4);
     }
 }
